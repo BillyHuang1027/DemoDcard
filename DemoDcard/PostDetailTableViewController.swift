3eb2b4d4120b74d4f48a122428195b1e48c735d5
Billy Huang <j28898601@gmail.com>
2021-05-11 11:44:32 +0800

    upgrade navigation item

--- DemoDcard/PostDetailTableViewController.swift
+++ DemoDcard/PostDetailTableViewController.swift
@@ -17,6 +17,7 @@ class PostDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = "\(post.title)"
         contentView.contentLabel.text = ""
         loadPostDetailData()
         loadCommentsData()
